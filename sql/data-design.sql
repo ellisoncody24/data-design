134e0a7356a4d66e935dd592390e739f4f26b9ec
Cody Ellison <ellison.cody24@gmail.com>
2020-05-06 16:04:49 -0600

    updated mysql

---
+++ sql/data-design.sql
@@ -0,0 +1,41 @@
+ALTER DATABASE cellison CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+
+
+
+
+
+
+
+drop table if exists itemOrder;
+drop table if exists `order`;
+drop table if exists item;
+drop table if exists `user`;
+
+
+create table `user`(
+                     userId binary(16) not null,
+                     userName varchar(32),
+                     userShippingInfo varchar(255),
+                     primary key(userId)
+);
+
+create table item(
+                    itemId binary(32) not null,
+                    itemQuantity varchar(32) not null,
+                    primary key(itemId)
+);
+
+create table `order`(
+                        orderId binary(16) not null,
+                        orderUserId binary(16) not null,
+                        orderDate datetime(6) not null,
+                        foreign key(orderUserId) references user(userId),
+                        primary key(orderId)
+);
+
+create table itemOrder(
+                           itemOrderItemId binary(16),
+                           itemOrderOrderId binary(16),
+                           foreign key(itemOrderItemId) references item(itemId),
+                           foreign key(itemOrderOrderId) references orders(orderId)
+);
